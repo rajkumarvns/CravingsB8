ec59ba882780344d9e41bb227b7d1df1a23a6810
rajkumarvns <rajkumar966601@gmail.com>
2026-06-14 14:55:32 +0530

    feat: add routing and components for Navbar, Footer, Login, Register, and ContactUs pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ContactUs from "./pages/ContactUs";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/contactUs" element={<ContactUs />} />
+          <Route path="/navbar" element={<Navbar />} />
         </Routes>
       </BrowserRouter>
     </>
